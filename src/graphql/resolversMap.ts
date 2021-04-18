4ac09f421866f0996e5b722e3b95710d1e4463b0
LeoSL <leosilvalima@gmail.com>
2021-04-18 15:13:26 -0400

    PubSub server working

--- src/graphql/resolversMap.ts
+++
@@ -1,7 +0,0 @@
-import { IResolvers } from 'graphql-tools'
-import { merge } from 'lodash'
-import { MarketDataResolvers } from './resolvers/MarketData'
-import { SubscriptionResolver } from './resolvers/Subscriptions'
-
-const resolverMap: IResolvers = merge(MarketDataResolvers, SubscriptionResolver)
-export default resolverMap
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,84 @@
 import express from 'express'
 import http from 'http'
-import { ApolloServer } from 'apollo-server-express'
+import { ApolloServer, gql, IResolvers } from 'apollo-server-express'
 import { PubSub } from 'apollo-server-express'
 
-import schema from './graphql/schemasMap'
-import { mockDataResponse } from './graphql/mockDB'
+import { QueryMarketDataArgs, MarketDataResponse } from './graphql/generated'
+import { filterArgsPerTickerPair } from './graphql/resolvers/MarketData'
+import {
+  generateRandomMarketDataResponse,
+  mockDataResponse,
+} from './graphql/mockDB'
+
+const pubsub = new PubSub()
 
 const generateMarketData = () => {
-  const pubsub = new PubSub()
+  const marketData = generateRandomMarketDataResponse()
+  console.log('inside Generation..........', JSON.stringify(marketData))
 
   pubsub.publish('DATA_GENERATED', {
-    marketData: mockDataResponse.tradingPairs,
+    marketData,
   })
 
   setTimeout(generateMarketData, 1000)
+}
+
+// TypeDefs --------------------
+
+const typeDefs = gql`
+  scalar JSON
+
+  type Query {
+    marketData(baseTicker: String!, quoteTicker: String!): MarketDataResponse!
+  }
+
+  type Mutation {
+    generateMarketData: MarketDataResponse!
+  }
+
+  type Subscription {
+    marketData: MarketDataResponse
+  }
+
+  type MarketDataResponse {
+    marketDataResponse: JSON
+  }
+`
+
+// Resolvers ----------------
+
+const MarketDataResolvers: IResolvers = {
+  Query: {
+    async marketData(
+      _: void,
+      args: QueryMarketDataArgs,
+    ): Promise<MarketDataResponse> {
+      const { baseTicker, quoteTicker } = args
+      const data = filterArgsPerTickerPair(
+        `${baseTicker}-${quoteTicker}`,
+        mockDataResponse.tradingPairs,
+      )
+
+      if (data == null || data.length === 0) {
+        console.error('❌ Error: Ticker Pair not found', args)
+        return {
+          marketDataResponse: undefined,
+        }
+      }
+
+      console.log('📚 Args', args)
+
+      return {
+        marketDataResponse: data,
+      }
+    },
+  },
+
+  Subscription: {
+    marketData: {
+      subscribe: () => pubsub.asyncIterator(['DATA_GENERATED']),
+    },
+  },
 }
 
 async function startApolloServer() {
@@ -21,7 +86,8 @@ async function startApolloServer() {
 
   const app = express()
   const server = new ApolloServer({
-    schema,
+    typeDefs,
+    resolvers: MarketDataResolvers,
     subscriptions: {
       path: '/subscriptions',
       onConnect: (connectionParams, webSocket, context) => {
@@ -34,21 +100,21 @@ async function startApolloServer() {
   })
 
   await server.start()
-  // server.applyMiddleware({ app, path: '/graphql' })
   server.applyMiddleware({ app })
 
   const httpServer = http.createServer(app)
   server.installSubscriptionHandlers(httpServer)
 
-  // Make sure to call listen on httpServer, NOT on app.
   // @ts-ignore
   await new Promise((resolve) => httpServer.listen(PORT, resolve))
+
   console.log(
     `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`,
   )
   console.log(
     `🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`,
   )
+
   generateMarketData()
 
   return { server, app, httpServer }
